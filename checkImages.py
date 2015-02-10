e0433a86c5d68d2fae1c107b8ba4eb6624b8dbc8
Jan Florian Dietrich <jfd@daenen4.de>
2015-02-10 10:08:03 +0100

    * Added ability to download images

--- checkImages.py
+++ checkImages.py
@@ -5,12 +5,17 @@ import pickle
 import os
 import json
 import logging
+import base64
+import io
+from PIL import Image
 
 baseDir = os.path.dirname(os.path.realpath(__file__))
 pickleFile = os.path.join(baseDir, 'ids.txt')
 solr = pysolr.Solr('http://vm-solrslave-02.lve-1.magic-technik.de:8080/myvideo-search/combined/', timeout=10)
+downloadDir = os.path.join(baseDir, 'imgs')
+do_download_files = True
 
-riakClient = RiakClient(protocol='http', host='10.228.39.113', http_port=8098)
+riakClient = RiakClient(protocol='http', host='vm-riak-04.lve-1.magic-technik.de', http_port=8098)
 riakBucket = riakClient.bucket('ez')
 
 uploadLogger = logging.getLogger('upload_logger')
@@ -24,8 +29,8 @@ uploadLogger.addHandler(fhu)
 
 def get_mams_ids():
     mamsIds = {}
-    results = solr.search('objectType:SERIES', **{
-        'rows': '1000'
+    results = solr.search('objectType:SERIES OR objectType:ARTIST', **{
+        'rows': '10000'
     })
     print (len(results))
     for result in results:
@@ -35,7 +40,7 @@ def get_mams_ids():
     return mamsIds
 
 def parse_bg_img(doc):
-    if doc['images'] and len(doc['images']) > 0:
+    if 'images' in doc and len(doc['images']) > 0:
         # print (doc['images'])
 
         for img_string in doc['images']:
@@ -46,8 +51,10 @@ def parse_bg_img(doc):
                     return path[3:]
                 else:
                     print('Doc %s has a background image that is not in riak %s' % (doc['id'], img['path'] ))
+                    return None
             else:
                 print ('Doc %s has no background image in solr. Title: %s' % (doc['id'], doc['title']))
+                return None
     else:
         print ('There are no images for doc %s' % doc['id'])
 
@@ -63,6 +70,16 @@ if not os.path.isfile(pickleFile):
 else:
     mamsIds = pickle.load(open(pickleFile, 'rb'))
 
+def save_image(path):
+    obj = riakBucket.get(path)
+    if obj.data is not None:
+        img = obj.data['imageBinary']
+        img = base64.b64decode(img)
+        imageFile = io.BytesIO(img)
+        imgObj = Image.open(imageFile)
+        imgObj.save(os.path.join(downloadDir, path), 'JPEG')
+
+
 
 missing = []
 for id, doc in mamsIds.iteritems():
@@ -76,6 +93,9 @@ for id, doc in mamsIds.iteritems():
         uploadLogger.info(id)
         print ('Missing Image %s for doc %s with title %s' % (bg_image, id, doc['title']))
         missing.append(bg_image)
+    elif bg_image is not None and img_exists(bg_image) is True and do_download_files is True:
+        save_image(bg_image)
+
 
 pickle.dump(missing, open(os.path.join(baseDir, 'missing.txt'), 'wb'))
 
